bcd8b78580068e53a1740063472695ef3dfca535
Narciso Meza <ncismeba@gmail.com>
2026-06-09 12:38:02 -0600

    feat: initial Xcode project setup and app entry point
    - Xcode project with PBXFileSystemSynchronizedRootGroup
    - R_MApp with LanguageManager injected into SwiftUI environment
    - ContentView placeholder

---
+++ R&M/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  R&M
+//
+//  Created by Narciso Meza on 09/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CharacterListView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ R&M/R_MApp.swift
@@ -0,0 +1,18 @@
+//
+//  R_MApp.swift
+//  R&M
+//
+//  Created by Narciso Meza on 09/06/26.
+//
+
+import SwiftUI
+
+@main
+struct R_MApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(LanguageManager.shared)
+        }
+    }
+}
